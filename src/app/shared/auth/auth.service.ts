216b3cd369bdad9c577badc86fea50192914d249
Stijn Dusseldorp <stijn@ardusit.nl>
2023-12-10 15:13:15 +0100

    Update auth.service.ts

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -11,14 +11,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
   
   register(user: User): Observable<any> {
-    return this.http.post('http://127.0.0.1:8000/api/auth/register', user);
+    return this.http.post('https://jukebox-backend-v2-05c77ef4e46e.herokuapp.com/api/auth/register', user);
   }
   // Login
   signin(user: User): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/auth/login', user);
+    return this.http.post<any>('https://jukebox-backend-v2-05c77ef4e46e.herokuapp.com/api/auth/login', user);
   }
   // Access user profile
   profileUser(): Observable<any> {
-    return this.http.get('http://127.0.0.1:8000/api/auth/user-profile');
+    return this.http.get('https://jukebox-backend-v2-05c77ef4e46e.herokuapp.com/api/auth/user-profile');
   }
 }
